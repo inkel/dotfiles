3cf58e17b9caa4d8b4847c60770000a6975038e6
Leandro López (inkel) <inkel.ar@gmail.com>
2012-09-09 12:12:09 -0300

    Replace install script

---
+++ install.sh
@@ -0,0 +1,42 @@
+#! /usr/bin/env bash
+
+ORIGIN=$(dirname $0)
+
+[[ -z $1 ]] && DST=$HOME || DST=$1
+
+echo "Installing dotfiles from ${ORIGIN} to ${DST}"
+
+DOTFILES="
+ackrc
+bash_login
+bashrc
+bashrc.d/colors
+gemrc
+gitconfig.sample
+gitignore
+profile
+pryrc
+tmux.conf
+"
+
+mkdir -p "${DST}/.bashrc.d"
+
+DIFF=$(which colordiff || which diff)
+
+for dotfile in $DOTFILES
+do
+    origin="${ORIGIN}/${dotfile}"
+    destination="${DST}/.${dotfile}"
+    if [[ "gitconfig.sample" == $dotfile ]]; then
+        destination="${DST}/.gitconfig"
+    fi
+
+    if [[ -f $destination || -L $destination ]]; then
+        echo "Skipping $origin: $destination exists"
+
+       [[ -f $destination ]] && $DIFF "$origin" "$destination"
+    else
+        echo "Symlinking $origin to $destination"
+        /bin/ls -s "$origin" "$destination"
+    fi
+done
